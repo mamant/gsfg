96d5e107309e6aa7c643d9a84996758543f875b2
mamant <ama@debitoor.com>
2017-03-05 09:47:44 +0200

    remove access token

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@
   const form = document.querySelector('[data-get-users]');
   let matchBtn = document.querySelector('[data-get-starred-btn]');
   const gitHubUsersAPI = 'https://api.github.com/users/';
-  const token = '';
+  const token = 'a897325b3659e0ccd9c0f45f6bad14752f45bc5b';
   let savedUsers = JSON.parse(localStorage.getItem('usersCollection')) || [];
   let usersURls = [];
   let usersCollection = [];
@@ -21,7 +21,7 @@
       this.name = user.name || 'User';
       this.login = user.login;
       this.avatar = user.avatar_url || user.avatar;
-      this.starredURL = `${user.url}/starred?per_page=100&access_token=${token}`
+      this.starredURL = `${user.url}/starred?per_page=100`
       this.starredRepositories = user.starredRepositories || [];
     }
 
